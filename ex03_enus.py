17e5a5710f1761c67308f6c5a3bfceac269950ad
ana.drmidnight@gmail.com <ana.drmidnight@gmail.com>
2026-08-20 15:32:16 -0300

    feat: Codificando a versão EN-US

---
+++ ex03_enus.py
@@ -0,0 +1,7 @@
+# Program that reads two numbers and displays their sum
+
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+total = n1 + n2
+
+print(f"The sum between {n1} and {n2} is {total}.")

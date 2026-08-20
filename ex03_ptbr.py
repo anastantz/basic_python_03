30d42d74d1a0e84ee634af493e3794ff1173e247
ana.drmidnight@gmail.com <ana.drmidnight@gmail.com>
2026-08-20 15:26:04 -0300

    feat: Codificando a versão PT-BR

---
+++ ex03_ptbr.py
@@ -0,0 +1,7 @@
+# Programa que lê dois números e mostra a soma entre eles
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+soma = n1 + n2
+
+print(f"A soma entre {n1} e {n2} é igual a {soma}.")
